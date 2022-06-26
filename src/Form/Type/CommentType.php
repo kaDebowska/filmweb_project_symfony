5fa8ec265d113b93cbf46c5991d17a1343c6add2
Katarzyna <you@example.com>
2022-06-26 19:33:47 +0000

    change field type in creating comment form

--- src/Form/Type/CommentType.php
+++ src/Form/Type/CommentType.php
@@ -8,6 +8,7 @@ namespace App\Form\Type;
 use App\Entity\Comment;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -32,7 +33,7 @@ class CommentType extends AbstractType
     {
         $builder->add(
             'content',
-            TextType::class,
+            TextareaType::class,
             [
                 'label' => 'label.content',
                 'required' => true,
